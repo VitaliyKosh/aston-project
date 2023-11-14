a176da94999800b793650617f7b64b190ac0e4cc
VitaliyKosh <zs.101101@yandex.ru>
2023-11-15 01:43:23 +0300

    untracked refactoring

--- src/repositories/user/api/errors.ts
+++ src/repositories/user/api/errors.ts
@@ -1,12 +1,8 @@
-import { AuthErrorCodes } from 'firebase/auth';
-import { type FirebaseError } from 'firebase/app';
-
 export class UserAuthError extends Error {
-    static types = AuthErrorCodes;
     type: string;
 
-    constructor (e: FirebaseError) {
-        super(e.message);
-        this.type = e.code;
+    constructor (code: string) {
+        super(code);
+        this.type = code;
     }
 }

faba6851c56f432f56fe0af085268f3864c4513a
VitaliyKosh <zs.101101@yandex.ru>
2023-11-15 17:03:39 +0300

    fix classNames

--- src/shared/lib/class-names/class-names.ts
+++ src/shared/lib/class-names/class-names.ts
@@ -1,11 +1,11 @@
 import type { ClassNames, ConditionalClassNames } from './types';
 
 export const classNames = (
-    classes: ClassNames | string | undefined,
+    classes: ClassNames | string,
     conditionalClasses?: ConditionalClassNames | undefined
 ): string => {
     return [
-        ...(typeof classes === 'string' ? [classes] : classes?.filter(c => c !== undefined) as string[]),
+        ...(typeof classes === 'string' ? [classes] : classes),
         ...(conditionalClasses
             ? Object.entries(conditionalClasses)
                 .filter(([_, flag]) => Boolean(flag))

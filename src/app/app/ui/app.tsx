6094a81554004148114db18349d1006021d15ed1
VitaliyKosh <zs.101101@yandex.ru>
2023-11-27 21:49:25 +0300

    fix app

--- src/app/app/ui/app.tsx
+++ src/app/app/ui/app.tsx
@@ -2,7 +2,7 @@ import './styles/index.scss';
 import { type RC } from 'shared/types/component';
 import { Suspense } from 'react';
 import { PageLoader } from 'widgets/page-loader';
-import { GlobalAppRouter } from './providers/app-router';
+import { GlobalAppRouter } from '../../providers/app-router';
 import { Provider } from 'react-redux';
 import { useAuth } from 'shared/hooks/use-auth';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,7 +10,7 @@ import { Header } from 'widgets/header';
 import { HelmetProvider } from 'react-helmet-async';
 import { getApp } from 'shared/helpers/get-app';
 import { ThemeProvider } from 'app/providers/theme-provider';
-import { FeatureFlagsProvider } from './providers/feature-flags-provider';
+import { FeatureFlagsProvider } from '../../providers/feature-flags-provider';
 
 export const App: RC = () => {
     const app = getApp();

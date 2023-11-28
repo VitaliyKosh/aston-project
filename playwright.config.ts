22583672cb8beb830bcf431c5c2f0f8243616874
VitaliyKosh <zs.101101@yandex.ru>
2023-11-29 01:03:33 +0300

    try to fix 7

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
+        baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3010',
         trace: 'on-first-retry'
     },
 
@@ -31,7 +31,7 @@ export default defineConfig({
 
     webServer: {
         command: 'npm run start',
-        url: 'http://localhost:3000',
+        url: 'http://localhost:3010',
         reuseExistingServer: !process.env.CI
     }
 });

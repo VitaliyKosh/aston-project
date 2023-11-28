c396dc253798e5051d87e6ea74a52da1177e88cd
VitaliyKosh <zs.101101@yandex.ru>
2023-11-29 01:32:45 +0300

    try to fix 11

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
+        baseURL: 'http://localhost:3000',
         trace: 'on-first-retry'
     },
 
--- tests/guest.spec.ts
+++ tests/guest.spec.ts
@@ -1,11 +1,30 @@
 import { test, expect } from '@playwright/test';
+import childProcess from 'child_process';
+
+let npmStart: childProcess.ChildProcessWithoutNullStreams;
+
+test.beforeAll(async () => {
+    await new Promise<void>((resolve, reject) => {
+        npmStart = childProcess.spawn('npm', ['start']);
+        npmStart.stdout.on('data', (data: string) => {
+            // console.log(`stdout: ${data}`);
+            if (data.includes('successfully')) {
+                resolve();
+            }
+        });
+    });
+});
+
+test.afterAll(async () => {
+    npmStart.kill();
+});
 
 test('guest test', async ({ page, baseURL }) => {
     if (!baseURL) {
         throw new Error('no base url');
     }
 
-    await page.goto('https://aston-iphone.netlify.app/');
+    await page.goto('http://localhost:3010');
 
     // await page.waitForTimeout(5000);
 
